4a96e6197ab86b41fc5a8384299c7cd7acceea4d
Abhishek <abhi.dtu11@gmail.com>
2025-03-10 21:26:05 -0700

    added dataset dir

--- train_gpt2.py
+++ train_gpt2.py
@@ -166,16 +166,16 @@ class DataLoaderLite:
         self.num_processes = num_processes
         assert split in {'train', 'val'}
         self.shards = sorted([
-            s for s in os.listdir(dataset_dir, "edu_fineweb10B") 
+            s for s in os.listdir(os.path.join(dataset_dir, "edu_fineweb10B"))            
             if s.startswith(f"edufineweb_{split}_") and s.endswith(".npy")
         ])
         self.current_position = self.process_rank * B * T
         self.split = split
-        self.data = load_token(dataset_dir, f"edu_fineweb10B/{self.shards[self.shard_id]}")
+        self.data = load_token(os.path.join(dataset_dir, f"edu_fineweb10B/{self.shards[self.shard_id]}"))
     
     def reset(self):
         self.shard_id = 0
-        self.data = load_token(dataset_dir, f"edu_fineweb10B/{self.shards[self.shard_id]}")
+        self.data = load_token(os.path.join(dataset_dir, f"edu_fineweb10B/{self.shards[self.shard_id]}"))
         self.current_position = self.process_rank * self.batch_size * self.context
         
     def next_batch(self):
@@ -196,7 +196,7 @@ class DataLoaderLite:
             > self.data.size(-1)
         ):
             self.shard_id = (self.shard_id + 1) % len(self.shards)
-            self.data = load_token(dataset_dir, f"edu_fineweb10B/{self.shards[self.shard_id]}")
+            self.data = load_token(os.path.join(dataset_dir, f"edu_fineweb10B/{self.shards[self.shard_id]}"))
             self.current_position = self.process_rank * self.batch_size * self.context
         return x, y
 
